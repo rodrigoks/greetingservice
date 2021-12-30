bd9139041a7dfbf8c52f18629bf93dafc09cf5cd
rodrigoks <rodrigoks@gmail.com>
2021-12-30 17:49:33 -0300

    Adicionados arquivos iniciais do projeto.

---
+++ src/test/java/br/com/jbusiness/GreetingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
